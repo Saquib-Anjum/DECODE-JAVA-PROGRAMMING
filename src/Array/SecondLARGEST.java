df713b02033767cc0e9af45fbc008de78356efb8
Saquib Anjum <saquibanjum8969@gmail.com>
2024-01-31 15:37:26 +0530

    Array and ArrayList

--- src/Array/SecondLARGEST.java
+++ src/Array/SecondLARGEST.java
@@ -2,7 +2,8 @@ package Array;
 
 public class SecondLARGEST {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8,789,898,98,897,98,789,6787};
+      int[] arr = {1,2,3,4,5,6,7,8,789,898,98,897,98,789,6787};
+       // int[] arr = {1,2,3,8,8,5,7,6};
         int max=Integer.MIN_VALUE;
         int secmax= Integer.MIN_VALUE;
         for (int i = 0; i <arr.length ; i++) {
