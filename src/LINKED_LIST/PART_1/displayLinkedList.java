3ed72badffc057612ed32679d4ef8293d973028a
Saquib Anjum <saquibanjum8969@gmail.com>
2024-03-28 22:43:35 +0530

    linkedList

---
+++ src/LINKED_LIST/PART_1/displayLinkedList.java
@@ -0,0 +1,14 @@
+package LINKED_LIST.PART_1;
+
+public class displayLinkedList {
+
+        public static void main(String[] args ){
+            Node a= new Node(100);
+            System.out.println(a.val);
+            Node temp=a;
+            System.out.println(temp.val);
+
+        }
+    }
+
+
