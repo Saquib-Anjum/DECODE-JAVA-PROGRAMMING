470c139165c79def9c78ee283d57efdb07c2916e
Saquib Anjum <saquibanjum8969@gmail.com>
2024-03-28 22:30:21 +0530

    Linked List

---
+++ src/LINKED_LIST/PART_1/DisplayLinkedList.java
@@ -0,0 +1,11 @@
+package LINKED_LIST.PART_1;
+
+public class DisplayLinkedList {
+    public static void main(String[] args ){
+        Node a= new Node(100);
+        System.out.println(a.val);
+        Node temp=a;
+        System.out.println(temp.val);
+
+    }
+}
--- src/LINKED_LIST/PART_1/linkedListIntro.java
+++ src/LINKED_LIST/PART_1/linkedListIntro.java
@@ -1,4 +1,36 @@
 package LINKED_LIST.PART_1;
 
 public class linkedListIntro {
+    public static void main(String[] args) {
+        Node a=new Node(10);
+        Node b=new Node(20);
+        Node c=new Node(30);
+        Node d=new Node(40);
+        Node e=new Node(50);
+        a.next=b;
+        b.next=c;
+        c.next=d;
+        d.next=e;
+        e.next=null;
+        Node temp=a;
+        //printing the linkedList
+//        System.out.println(a.val);
+//        System.out.println(b.val);
+//        System.out.println(c.val);
+//        System.out.println(d.val);
+//        System.out.println(e.val);
+        for (int i = 0; i < 5; i++) {
+            System.out.println(temp.val);
+            temp=next.temp;
+        }
+
+    }
+
+}
+class Node{
+ int val;
+Node next;
+ Node(int val){
+    this.val=val;
+}
 }
